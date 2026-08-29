f9100d49e92b74d3d7da3017fc5f6c73c2ec4441
qasimliyusif1918 <y9ymh5tjzn@privaterelay.appleid.com>
2026-08-29 23:32:38 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'waterdrop-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).catch(() => cached);
+    })
+  );
+});
